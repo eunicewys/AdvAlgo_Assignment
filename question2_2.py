1c74a766914ac7fa62850e6d52311a53927c7667
HAHAHA <p22014532@student.newinti.edu.my>
2025-06-28 13:59:57 +0800

    Add files via upload

--- question2_2.py
+++ question2_2.py
@@ -1,8 +1,5 @@
 class Person:
     def __init__(self, name, gender, bio, privacy="public"):
-        """
-        Create a new Person with basic social media profile information.
-        """
         self.name = name
         self.gender = gender
         self.bio = bio
