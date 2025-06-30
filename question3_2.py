d9da725e30e4f9dcadf9d611d6a6df6531e8a3fe
HAHAHA <p22014532@student.newinti.edu.my>
2025-06-30 20:11:35 +0800

    Add files via upload

--- question3_2.py
+++ question3_2.py
@@ -1,13 +1,14 @@
 import random
 
-def generate_random_numbers():
-    numbers = [] 
+def generate_sorted_random_numbers():
+    numbers = []
     for _ in range(100):
         num = random.randint(0, 10000)
         numbers.append(num)
+    numbers.sort()
     return numbers
 
-random_list = generate_random_numbers()
+sorted_random_list = generate_sorted_random_numbers()
 
-print("Generated 100 random numbers:")
-print(random_list)
+print("100 Sorted Random Numbers (0 to 10,000):")
+print(sorted_random_list)
